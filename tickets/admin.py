19497d68e90fe0567899b7e500002d8ac30ecfab
Jerry <ty010777@gmail.com>
2021-07-14 14:48:05 +0800

    jerry
    資料庫測試..
    尚未完成models裡的清單
    PS. 完全不知道為什麼成功

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Movie
 
 # Register your models here.
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'release_year')
+
+
+admin.site.register(Movie, MovieAdmin)
--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Movie(models.Model):
+    GENRE_CHOICES = (
+        ('action', '動作片'),
+        ('romance', '愛情片'),
+        ('drama', '劇情片'),
+    )
+    title = models.CharField(max_length=100)  # 電影名稱
+    genre = models.CharField(max_length=20, choices=GENRE_CHOICES)  # 電影類型
+    release_year = models.IntegerField()  # 年份
